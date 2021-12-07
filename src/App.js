9cb8618d4dd258ac3b8903c631be1074fd11e4cf
Ernest Bieś <ernestbies@gmail.com>
2021-12-07 23:25:34 +0100

    Project changes.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import {Route} from 'react-router';
 import {HashRouter, Routes} from 'react-router-dom';
 import Home from "./pages/Home";
 
-const App = () => {
-    return (
-        <HashRouter>
-            <Routes>
-                <Route path={'*'} element={<Home/>}/>
-            </Routes>
-        </HashRouter>
-    );
-}
+const App = () => (
+    <HashRouter>
+        <Routes>
+            <Route path={'*'} element={<Home/>}/>
+        </Routes>
+    </HashRouter>
+);
 
 export default App;
